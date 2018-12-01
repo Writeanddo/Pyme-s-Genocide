16bbb8504cf667db10fb2b9975ca5c4191644fdc
AedonStorm <aedon20@gmail.com>
2018-12-02 00:49:21 +0100

    Force Shield

---
+++ Assets/Scripts/Obstacles/CrystalPower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalPower : MonoBehaviour {
+
+    private int life = 5;
+    private bool destroyed = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Minion")
+        {
+            life--;
+            Destroy(collision.gameObject);
+            if (!destroyed && life <= 0)
+            {
+                destroyed = true;
+                GetComponentInParent<ForceShield>().CrystalDestroyed();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/ForceShield.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceShield : MonoBehaviour {
+
+    private int power;
+
+	// Use this for initialization
+	void Start () {
+        power = transform.childCount;
+    }
+	
+    public void CrystalDestroyed()
+    {
+        power--;
+        if (power <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
