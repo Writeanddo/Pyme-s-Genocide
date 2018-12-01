b2b78630dccc0cf34d1bdf1674b924f3c9813777
Masacre23 <adrianguevaralonso@gmail.com>
2018-12-01 18:25:46 +0100

    Now you can shot

--- Assets/Scripts/Minions/Absorber.cs
+++ Assets/Scripts/Minions/Absorber.cs
@@ -7,6 +7,20 @@ public class Absorber : MonoBehaviour {
     public int force = 100;
     public enum Type { detele, force };
     public Type type;
+    public GameObject bullet;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0) && type == Type.force) {
+            GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
+            newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * force * 20);
+        }
+
+        /* if (Input.GetMouseButtonDown(1))
+             Debug.Log("Pressed secondary button.");
+
+         if (Input.GetMouseButtonDown(2))
+             Debug.Log("Pressed middle click.");*/
+    }
 
     private void OnTriggerStay(Collider other) {
         if (type == Type.force) {
