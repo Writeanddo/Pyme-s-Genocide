efa69a99a42e7008248dc7d14146bc91e3e60162
AedonStorm <aedon20@gmail.com>
2018-12-01 17:51:10 +0100

    Jumper

---
+++ Assets/Scripts/Obstacles/Jumper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Jumper : MonoBehaviour {
+
+    public float power = 150f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var rigidbody = other.gameObject.GetComponent<Rigidbody>();
+        if (rigidbody) rigidbody.AddForce(transform.forward * power, ForceMode.Impulse);
+    }
+}
