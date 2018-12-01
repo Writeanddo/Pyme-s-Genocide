6dd7360e1f02d9ea1c8e1ee5c8b79526f6522c49
Masacre23 <adrianguevaralonso@gmail.com>
2018-12-01 17:08:38 +0100

    Absorbedor

---
+++ Assets/Scripts/Minions/Absorber.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Absorber : MonoBehaviour {
+
+    public int force = 100;
+    public GameObject placeholder;
+
+    private void Update() {
+    //    transform.position = placeholder.transform.position;
+    //    transform.rotation
+    }
+
+    private void OnTriggerStay(Collider other) {
+        Vector3 heading = other.transform.position - transform.position;
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+        if(r) r.AddForce(heading * -force);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        print("hola?");
+        collision.gameObject.SetActive(false);
+    }
+}
