aa0e628955eb9ad28bdd13ad3d892aa927f09215
AedonStorm <aedon20@gmail.com>
2018-12-01 14:21:04 +0100

    Plataforma en movimiento y plataforma que se cae

---
+++ Assets/Scripts/Obstacles/DropPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DropPlatform : MonoBehaviour {
+
+    private float cooldown = 0.5f;
+    private bool droped = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Invoke("Drop", cooldown);
+    }
+
+    private void Drop()
+    {
+        if (!droped)
+        {
+            droped = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+            Destroy(gameObject, 5f);
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/PlatformMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    public List<Vector3> childPositions;
+
+    public float velocity = 0.5f;
+    private int currentPosition = 0;
+    private int nextPosition;
+    private bool fordwardMovement = true;
+    private float distanceThreshold = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        foreach (Transform child in transform)
+        {
+            childPositions.Add(child.position);
+        }
+
+        if (childPositions.Count > 1) nextPosition = currentPosition + 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, childPositions[nextPosition], velocity * Time.deltaTime);
+        UpdateCurrentPosition();
+    }
+
+    private void UpdateCurrentPosition()
+    {
+        if(Vector3.Distance(transform.position, childPositions[nextPosition]) < distanceThreshold)
+        {
+            CalculateNextPosition();
+        }
+    }
+
+    private void CalculateNextPosition()
+    {
+        currentPosition = nextPosition;
+
+        if (fordwardMovement)
+        {
+            if (currentPosition == childPositions.Count - 1)
+            {
+                fordwardMovement = false;
+                nextPosition--;
+            }
+            else nextPosition++;
+        }
+        else
+        {
+            if (currentPosition == 0)
+            {
+                fordwardMovement = true;
+                nextPosition++;
+            }
+            else nextPosition--;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent = transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+
+
+
+
+}
