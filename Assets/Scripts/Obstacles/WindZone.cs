0cb867b82630151dbfb2e48ba09603d467494f85
AedonStorm <aedon20@gmail.com>
2018-12-01 16:55:01 +0100

    Planks, Jumper, WindZone

---
+++ Assets/Scripts/Obstacles/WindZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindZone : MonoBehaviour {
+    
+    private float minPower = 0;
+    public float maxPower = 2f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        var rigidbody = other.GetComponent<Rigidbody>();
+        if (rigidbody) rigidbody.velocity += transform.forward * Random.Range(minPower, maxPower) * Time.deltaTime;
+    }
+}
