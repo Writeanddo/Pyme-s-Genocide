493b343b89f5fa8cc92fcecabf298cb11a95c81c
AedonStorm <aedon20@gmail.com>
2018-12-01 21:21:55 +0100

    base UI, base AudioManager

---
+++ Assets/Scripts/Controllers/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    private static AudioManager instance;
+    public static AudioManager GetInterfaceController() { return instance; }
+
+    [Header("AmbientMusic")]
+    public AudioClip ambienMusic;
+
+    //[Header("Scene")]
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Controllers/InterfaceController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceController : MonoBehaviour {
+
+    private static InterfaceController instance;
+    public static InterfaceController GetInterfaceController() { return instance; }
+
+    public Text textCount;
+    public Image imageCount;
+
+	
+	void Awake () {
+        instance = this;
+	}
+
+    public void UpdateCounter(float count)
+    {
+        textCount.text = (int)count + "%";
+        imageCount.fillAmount = count/100f;
+    }
+}
