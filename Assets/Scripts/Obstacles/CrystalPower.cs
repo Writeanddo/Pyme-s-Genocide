797113a5e7250cf70619d9dfb7ec2bdb9cd4ca40
AedonStorm <aedon20@gmail.com>
2018-12-04 00:10:42 +0100

    Forcefield puzzle + Rock&Roll

--- Assets/Scripts/Obstacles/CrystalPower.cs
+++ Assets/Scripts/Obstacles/CrystalPower.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CrystalPower : MonoBehaviour {
 
+    public GameObject particleDamage;
+    public GameObject particleDestroy;
     private int life = 5;
     private bool destroyed = false;
 
@@ -12,11 +14,13 @@ public class CrystalPower : MonoBehaviour {
         if (collision.gameObject.tag == "Minion")
         {
             life--;
+            Instantiate(particleDamage, transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
             if (!destroyed && life <= 0)
             {
                 destroyed = true;
                 GetComponentInParent<ForceShield>().CrystalDestroyed();
+                Instantiate(particleDestroy, transform.position, Quaternion.identity);
                 Destroy(gameObject);
             }
         }
